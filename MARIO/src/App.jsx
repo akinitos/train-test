5a97e447581da278bed3b4aec73a71cd966ae36f
akinitos <vonarcibal@gmail.com>
2026-02-24 18:25:07 +0800

    setup

---
+++ MARIO/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Landing from "./pages/landing.jsx"
+
+function App() {
+  return (
+    <>
+     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route index element={<Landing />} />
+
+        <Route path="landing" element={<Landing />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+  )
+}
+
+export default App;
---
+++ MARIO/src/pages/landing.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+import "../styles/landing.css"
+
+function Landing() {
+    return (
+        <div className="mainContainer">
+            
+        </div>
+    )
+}
